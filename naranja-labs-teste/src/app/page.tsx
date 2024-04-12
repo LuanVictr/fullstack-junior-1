dfd43f41460c43c5f82e28ec6e71e8a228e75ca8
Luan Victor <>
2024-04-12 13:29:38 -0300

    fix: fixing page not getting on fullscreen

--- naranja-labs-teste/src/app/page.tsx
+++ naranja-labs-teste/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="flex flex-col overflow-hidden">
+   <div className="flex flex-col w-screen">
     <Header />
     <Body />
    </div>
